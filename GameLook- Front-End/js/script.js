adc2b6cb9fce8600dc639dcd2ea7aca2a7e5773e
Enes <enesgndgdu13@gmail.com>
2024-05-19 15:58:25 +0300

    Oyun Satış Sitesi Front-End Tasarımı
    Bir Oyun Satış Sitesinin Front-End Tasarımı. İçerisinde; anasayfa, oyunlar, iletişim, hakkımızda ve profil sayfası vardır.

---
+++ GameLook- Front-End/js/script.js
@@ -0,0 +1,65 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        320: {
+            slidesPerView: 2,
+        },
+        750: {
+          slidesPerView: 3,
+        },
+        950: {
+          slidesPerView: 4,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".slide-content-2", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination-2",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next-2",
+      prevEl: ".swiper-button-prev-2",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        320: {
+            slidesPerView: 2,
+        },
+        750: {
+          slidesPerView: 3,
+        },
+        950: {
+          slidesPerView: 4,
+      },
+    },
+  });
